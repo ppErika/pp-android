8098a64935b96ec52ae93f7bbf6223de9ed40793
eunjeong Lee <tbig1019@gmail.com>
2024-05-01 17:54:54 +0900

    kakao init

--- app/src/main/java/com/pp/pp/di/HiltApplication.kt
+++ app/src/main/java/com/pp/pp/di/HiltApplication.kt
@@ -1,11 +1,14 @@
 package com.pp.pp.di
 
 import android.app.Application
+import com.kakao.sdk.common.KakaoSdk
+import com.pp.pp.BuildConfig
 import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
 class HiltApplication: Application() {
     override fun onCreate() {
         super.onCreate()
+        KakaoSdk.init(this,BuildConfig.KAKAO_API_KEY)
     }
 }
