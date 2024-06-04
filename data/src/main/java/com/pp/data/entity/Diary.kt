5e4cdd192ca1ec66baee1bc1a3a8642e9cbeec06
Erika_Han <hanerika11@gmail.com>
2024-06-04 22:10:19 +0900

    update databse, entity, dao

--- data/src/main/java/com/pp/data/entity/Diary.kt
+++ data/src/main/java/com/pp/data/entity/Diary.kt
@@ -5,9 +5,9 @@ import androidx.room.Entity
 import androidx.room.PrimaryKey
 import org.w3c.dom.Text
 
-@Entity
+@Entity(tableName = "diary")
 data class Diary(
-    @PrimaryKey val id: Int,
+    @PrimaryKey @ColumnInfo(name = "id") val id: Int,
     @ColumnInfo(name = "title") val title: String,
     @ColumnInfo(name = "contents") val contents: String,
     @ColumnInfo(name = "create_date") val createDate: String,
--- data/src/main/java/com/pp/data/repository/PpApiRepositoryImpl.kt
+++ data/src/main/java/com/pp/data/repository/PpApiRepositoryImpl.kt
@@ -1,19 +1,32 @@
 package com.pp.data.repository
 
+import androidx.annotation.WorkerThread
+import com.pp.data.dao.DiaryDao
 import com.pp.data.datasource.server.PpApiDataSource
+import com.pp.data.entity.Diary
 import com.pp.domain.model.token.OauthTokenRequest
 import com.pp.domain.model.token.OauthTokenResponse
 import com.pp.domain.repository.PpApiRepository
 import com.pp.domain.utils.RemoteError
+import kotlinx.coroutines.flow.Flow
 import javax.inject.Inject
 
 class PpApiRepositoryImpl @Inject constructor(
+    private val diaryDao: DiaryDao,
     private val ppApiDataSource: PpApiDataSource
 ) : PpApiRepository {
+    val allDiaries: List<Diary> = diaryDao.getAll()
+
     override suspend fun oauthToken(
         remoteError: RemoteError,
         oauthTokenRequest: OauthTokenRequest
     ): OauthTokenResponse? {
         return ppApiDataSource.oauthToken(remoteError, oauthTokenRequest)
+    }
+
+    @Suppress("RedundantSuspendModifier")
+    @WorkerThread
+    suspend fun insert(diary: Diary) {
+        diaryDao.insert(diary)
     }
 }
