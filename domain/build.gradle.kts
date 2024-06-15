2e0c14b4fd13697e5e6c8ba2fa452cef8c05be60
Erika_Han <hanerika11@gmail.com>
2024-06-15 20:45:58 +0900

    Troubleshooting Circular Reference Errors

--- domain/build.gradle.kts
+++ domain/build.gradle.kts
@@ -38,7 +38,6 @@ dependencies {
     implementation(libs.core.ktx)
     implementation(libs.appcompat)
     implementation(libs.material)
-    implementation(project(":data"))
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.test.ext.junit)
     androidTestImplementation(libs.espresso.core)
